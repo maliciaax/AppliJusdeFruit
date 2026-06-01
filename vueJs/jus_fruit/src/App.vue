732e6c622d7be9f431727892f1120a57b5df6e21
Maliciaax <127119818+maliciaax@users.noreply.github.com>
2026-06-01 14:11:03 +0200

    ajout menu

--- vueJs/jus_fruit/src/App.vue
+++ vueJs/jus_fruit/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <menuPublic></menuPublic>
     <button @click="formulaire('ajouter')">
       <span>Ajouter Gare</span>
     </button>
@@ -13,6 +14,7 @@
 </template>
 
 <script>
+import menuPublic from './components/menu.vue'
 import gareComposent from './components/gare.vue'
 import ajouterGare from './components/ajouter.vue'
 import modifierGare from './components/modifier.vue'
@@ -20,6 +22,7 @@ import modifierGare from './components/modifier.vue'
 export default {
   name: 'App',
   components: {
+    menuPublic,
     gareComposent,
     ajouterGare,
     modifierGare
@@ -60,12 +63,76 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  body{
+      margin: 0;
+  }
+
+  nav {
+      display: flex; align-items: center;
+      position: sticky; top: 0; z-index: 100;
+      justify-content: space-between;
+      padding: 0.8rem 1.5rem;
+      background-color: rgb(46, 46, 46);
+      box-shadow: 0 2px 8px rgba(0,0,0,0.3);
+  }
+
+  .navlink {
+      display: flex; gap: 1.5rem;
+      list-style: none; margin: 0; padding: 0;
+  }
+
+  nav a { 
+      text-decoration: none; 
+      color: #f3f3f3; 
+  }
+
+  nav a:hover, nav a:focus { 
+      color: #86b86a; 
+      outline: none; 
+  }
+
+  nav a:focus-visible { 
+      outline: 2px solid #86b86a; 
+      border-radius: 3px; 
+  }
+
+  .imgLogo { 
+      width: 13rem; 
+  }
+
+  .boutonLogin {
+      background-color: #578040;
+      padding: 0.45rem 1.1rem;
+      border-radius: 0.5rem;
+      color: #f3f3f3;
+      border: none; cursor: pointer;
+      font-family: 'Montserrat', sans-serif;
+      font-size: 0.95rem; font-weight: 500;
+      text-decoration: none;
+      transition: background-color 0.2s;
+  }
+
+  .boutonLogin:hover { 
+      background-color: rgb(70,106,50); 
+      color: #f3f3f3!important;
+  }
+
+  .boutonLogin:focus-visible { 
+      outline: 2px solid #f3f3f3; 
+      outline-offset: 2px; 
+  }
+
+  #filtre .boutonLogin.actif {
+      background-color: rgb(70,106,50);
+      outline: 2px solid #f3f3f3;
+  }
 </style>
--- vueJs/jus_fruit/src/components/menu.vue
+++ vueJs/jus_fruit/src/components/menu.vue
@@ -1,42 +1,14 @@
 <template>
-    <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <nav>
+        <a href="/peguy/appliVegan/index.php" class="nav-logo" alt="logoSite"><img class="imgLogo" src="/peguy/appliVegan/img/logoLong.png"></a>
+        <ul class="navlink">
+            <li><a href="/peguy/appliVegan/index.php">Accueil</a></li>
+            <li><a href="/peguy/appliVegan/magasin.php">Magasins</a></li>
+            <li><a href="/peguy/appliVegan/contact.php">Contact</a></li>
+            <li><a href="/peguy/appliVegan/connexionUtilisateur-Form.php" class="boutonLogin">Se connecter</a></li>
+            <li><a href="/peguy/appliVegan/creerCompteUtilisateur-Form.php" class="boutonLogin">Créer un compte</a></li>
+        </ul>
+    </nav>
 
 </template>
 
